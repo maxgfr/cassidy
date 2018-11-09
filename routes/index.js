8cc983fba885ef2d177c3d280d7c88f4342d62fe
Maxime Golfier <maxgolfier@gmail.com>
2018-11-09 15:32:34 +0100

    maxime_commit

--- routes/index.js
+++ routes/index.js
@@ -15,16 +15,13 @@ router.get('/', function(req, res, next) {
     res.render('error');
   } else {
     console.log("Conversation initialisée");
-    res.render('chatbot/index', {
+    res.render('index', {
       conversation: conversation
     });
   }
 });
 
-
 router.post('/', function(req, res, next) {
-  //console.log(req.body.input);
-  //console.log(context);
   conversation.message({
     input: {
       text: req.body.input
@@ -50,160 +47,56 @@ router.post('/', function(req, res, next) {
         if (response.entities[0].entity == 'color') {
           type = "display_car";
           var color = response.entities[0].value;
-          name_image = 'images/Access' + color + '.jpg';
+          name_image = 'images/5008Access' + color + '.jpg';
         }
       }
+      type = "display_car";
+      name_image = 'images/5008AccessGreen.jpg';
       res.send([response, type, name_image]);
     }
   });
 });
 
 
-function postV2() {
-  router.post('/', function(req, res, next) {
+router.post('/giveinfo', function(req, res, next) {
+  console.log(req.body);
 
-    if (!req.body.input) {
-      macrocontext.init()
-    }
-    var prompt2 = macrocontext.processResponse(req.body.input)
-
-    var text = prompt2.output ? prompt2.output : 'We are done! Are you ready to order?'
-
-    var response = {
-      'output': {
-        'text': [text]
-      }
-    }
-
-    var type = 'nothing'
-    var image = ''
-
-    if (prompt2.options) {
-      console.log('prompt2.options:')
-        console.log(prompt2.options)
-      type = 'display_car'
-      image = {
-        'name_image': 'images/' + prompt2.options.image + '.jpg',
-        'justifications': [{
-          'text': prompt2.options.model.value,
-          'justification': prompt2.options.model.justification
-        }, {
-          'text': prompt2.options.trim_level.value,
-          'justification': prompt2.options.trim_level.justification
-        }, {
-          'text': prompt2.options.color.value,
-          'justification': prompt2.options.color.justification
-        }, {
-          'text': prompt2.options.price.value,
-          'justification': prompt2.options.price.justification
-        }, {
-          'text': prompt2.options.delay.date,
-          'justification': prompt2.options.delay.justification
-        }]
-      }
-    }
-
-    // if (text.includes('What is your top priority')) { // // TODO: Improve
-    if (text.includes('What is important to you?')) {
-      console.log('******* YES ********>')
-      type = "find_priority"
-    }
-
-    res.send([response, type, image]);
+  var value_security = req.body['input[Safety]'];
+  var value_comfort = req.body['input[Comfort_and_Convenience]'];
+  var value_vitesse = req.body['input[In_Car_Entertainment_and_Communication]'];
+  var value_style = req.body['input[style]'];
+  var dictionary = {
+    "security": value_security,
+    "comfort": value_comfort,
+    "speed": value_vitesse,
+    "style": value_style
+  };
+  // Create items array
+  var items = Object.keys(dictionary).map(function(key) {
+    return [key, dictionary[key]];
   });
-}
-
-postV2()
-
-function giveinfo1() {
-  router.post('/giveinfo', function(req, res, next) {
-    console.log(req.body);
-
-    var value_security = req.body['input[Safety]'];
-    var value_comfort = req.body['input[Comfort_and_Convenience]'];
-    var value_vitesse = req.body['input[In_Car_Entertainment_and_Communication]'];
-    var value_style = req.body['input[style]'];
-    var dictionary = {
-      "security": value_security,
-      "comfort": value_comfort,
-      "speed": value_vitesse,
-      "style": value_style
-    };
-    // Create items array
-    var items = Object.keys(dictionary).map(function(key) {
-      return [key, dictionary[key]];
-    });
-    // Sort the array based on the second element
-    items.sort(function(first, second) {
-      return second[1] - first[1];
-    });
-    console.log(items);
-    var value_max = items[0][0];
-    console.log(value_max);
-    conversation.message({
-      input: {
-        text: value_max
-      },
-      context: context_array[context_array.length - 1],
-      workspace_id: '635a4d6e-022d-4e16-88d6-4844c2cdcc99'
-    }, function(err, response) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(response);
-        res.send([response, "", ""]);
-      }
-    });
-
+  // Sort the array based on the second element
+  items.sort(function(first, second) {
+    return second[1] - first[1];
   });
-}
-
-function giveinfo2() {
-  router.post('/giveinfo', function(req, res, next) {
-    // console.log(req.body);
-    /*
-    var value_security = req.body['input[security]'];
-    var value_comfort = req.body['input[confort]'];
-    var value_vitesse = req.body['input[vitesse]'];
-    var value_style = req.body['input[style]'];
-    var dictionary = {
-      "security": value_security,
-      "comfort": value_comfort,
-      "speed": value_vitesse,
-      "style": value_style
-    };
-    // Create items array
-    var items = Object.keys(dictionary).map(function(key) {
-      return [key, dictionary[key]];
-    });
-    // Sort the array based on the second element
-    items.sort(function(first, second) {
-      return second[1] - first[1];
-    });
-    console.log(items);
-    */
-
-console.log(req.body)
-console.log(req.body['input[Safety]'])
-// macrocontext.ratings = req.body
-// macrocontext.setMicrogoalValue(goals.MICROGOALratings, req.body)
-// console.log(macrocontext.ratings['input[Safety]'])
-    var prompt2 = macrocontext.processResponse(req.body)
-
-    var text = prompt2.output ? prompt2.output : 'We are done! Are you ready to order?'
-
-    var response = {
-      'output': {
-        'text': [text]
-      }
+  console.log(items);
+  var value_max = items[0][0];
+  console.log(value_max);
+  conversation.message({
+    input: {
+      text: value_max
+    },
+    context: context_array[context_array.length - 1],
+    workspace_id: '635a4d6e-022d-4e16-88d6-4844c2cdcc99'
+  }, function(err, response) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(response);
+      res.send([response, "", ""]);
     }
-    var type = 'nothing'
-    var image = ''
+  });
 
-    res.send([response, type, image]);
-  })
-}
-
-giveinfo2()
+});
 
 module.exports = router;
