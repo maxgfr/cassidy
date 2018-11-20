a5be3335b88519a784b8aab9805ad6bd6c3db765
Maxime Golfier <maxgolfier@gmail.com>
2018-11-20 18:04:01 +0100

    maxime_commit

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var context_array = [];
 var number = 0;
 var context = null;
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   context_array = [];
   number = 0;
@@ -22,79 +21,62 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  conversation.message({
-    input: {
-      text: req.body.input
-    },
-    context: context_array[context_array.length - 1],
-    workspace_id: '635a4d6e-022d-4e16-88d6-4844c2cdcc99'
-  }, function(err, response) {
-    if (err) {
-      console.error(err);
-    } else {
-      var type = 'nothing';
-      var name_image = '';
-      console.log(response);
-      //var rep = response.output.text;
-      context = response.context;
-      context_array[number] = response.context;
-      number++;
-      if (response.output.text == 'What is your priority ?') {
-        type = "find_priority";
+  var message = req.body.input;
+  var result = makeCloudantRequest(req.body);
+  if(result.length != 1) {
+    console.log('Not found');
+    conversation.message({
+      input: {
+        text: msg
+      },
+      context: context_array[context_array.length - 1],
+      workspace_id: '635a4d6e-022d-4e16-88d6-4844c2cdcc99'
+    }, function(err, response) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(response);
+        var rep = response.output.text[0];
+        context = response.context;
+        context_array[number] = response.context;
+        number++;
+        res.send([rep, '', {}]);
       }
-      if (response.entities[0]) {
-        //console.log(response.entities[0].entity);
-        if (response.entities[0].entity == 'color') {
-          type = "display_car";
-          var color = response.entities[0].value;
-          name_image = 'images/5008Access' + color + '.jpg';
+    });
+  }
+  else {
+    console.log('Found');
+    res.send(["We found a car which matches your expectation !", '', result]);
+  }
+
+});
+
+function makeCloudantRequest(params) {
+    var from = params["from"];
+    var to = params["to"];
+    delete params["input"];
+    delete params["from"];
+    delete params["to"];
+    console.log(params);
+    var data = [];
+    if(!mydb) {
+      return data;
+    }
+    mydb.find({ selector: params }, function(err, result) {
+      if (!err) {
+        //console.log('Found %d documents', result.docs.length);
+        for (var i = 0; i < result.docs.length; i++) {
+          if (result.docs[i].prix < to && result.docs[i].prix > from) {
+            //console.log(result.docs[i])
+            data.push(result.docs[i])
+          }
         }
+        return data;
+      } else {
+        console.log(err);
+        return data;
       }
-      res.send([response, type, name_image]);
-    }
-  });
-});
-
-
-router.post('/giveinfo', function(req, res, next) {
-  console.log(req.body);
-
-  var value_security = req.body['input[Safety]'];
-  var value_comfort = req.body['input[Comfort_and_Convenience]'];
-  var value_vitesse = req.body['input[In_Car_Entertainment_and_Communication]'];
-  var value_style = req.body['input[style]'];
-  var dictionary = {
-    "security": value_security,
-    "comfort": value_comfort,
-    "speed": value_vitesse,
-    "style": value_style
-  };
-  // Create items array
-  var items = Object.keys(dictionary).map(function(key) {
-    return [key, dictionary[key]];
-  });
-  // Sort the array based on the second element
-  items.sort(function(first, second) {
-    return second[1] - first[1];
-  });
-  console.log(items);
-  var value_max = items[0][0];
-  console.log(value_max);
-  conversation.message({
-    input: {
-      text: value_max
-    },
-    context: context_array[context_array.length - 1],
-    workspace_id: '635a4d6e-022d-4e16-88d6-4844c2cdcc99'
-  }, function(err, response) {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(response);
-      res.send([response, "", ""]);
-    }
-  });
-
-});
+    });
+}
 
 module.exports = router;
